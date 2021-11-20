697e1131845c5b4a0520df702eb108ac7f81d31e
Leonardo Magno Marones Lopes <leonardo.marones@mobicare.com.br>
2021-11-20 17:21:23 -0300

    - Aula 4 - Condicionais para o tratamento das views caso seja um item do tipo despesa ou receita.

--- app/src/main/java/alura/com/financask/ui/adapter/ListaTransacoesAdapter.kt
+++ app/src/main/java/alura/com/financask/ui/adapter/ListaTransacoesAdapter.kt
@@ -2,12 +2,14 @@ package alura.com.financask.ui.adapter
 
 import alura.com.financask.R
 import alura.com.financask.extensions.formataParaBrasileiro
+import alura.com.financask.model.Tipo
 import alura.com.financask.model.Transacao
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.transacao_item.view.*
 
 class ListaTransacoesAdapter(
@@ -32,6 +34,15 @@ class ListaTransacoesAdapter(
     override fun getView(position: Int, view: View?, parent: ViewGroup?): View {
         val viewCriada = LayoutInflater.from(context).inflate(R.layout.transacao_item, parent, false)
         val transacao = transacoes[position]
+
+        if (transacao.tipo == Tipo.RECEITA) {
+            viewCriada.transacao_valor.setTextColor(ContextCompat.getColor(context, R.color.receita))
+            viewCriada.transacao_icone.setBackgroundResource(R.drawable.icone_transacao_item_receita)
+        } else {
+            viewCriada.transacao_valor.setTextColor(ContextCompat.getColor(context, R.color.despesa))
+            viewCriada.transacao_icone.setBackgroundResource(R.drawable.icone_transacao_item_despesa)
+        }
+
         viewCriada.transacao_valor.text = transacao.valor.toString()
         viewCriada.transacao_categoria.text = transacao.categoria
         viewCriada.transacao_data.text = transacao.data.formataParaBrasileiro()
